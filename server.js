e7539fdbddff38a65a65b2b5a4fd3c9ddf66a279
Ernesto Laval <ernesto.laval@gmail.com>
2017-06-04 20:32:48 +0100

    Option with no https (for dev)
    ENV VARIABLE "NO_HTTPS": "true"

--- server.js
+++ server.js
@@ -73,65 +73,70 @@ app.get('/matricula', function (req, res) {
   regionHandlers.matriculaHandler(req,res);
 });
 
-/*
+if (process.env.MONGO_DATABASE && process.env.MONGO_DATABASE === 'true') {
+ 
+  // returns an instance of node-greenlock with additional helper methods 
+  var lex = require('greenlock-express').create({
+    // set to https://acme-v01.api.letsencrypt.org/directory in production 
+    server: 'https://acme-v01.api.letsencrypt.org/directory'
+  
+  // If you wish to replace the default plugins, you may do so here 
+  // 
+  , challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
+  , store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
+  
+  // You probably wouldn't need to replace the default sni handler 
+  // See https://git.daplie.com/Daplie/le-sni-auto if you think you do 
+  //, sni: require('le-sni-auto').create({}) 
+  
+  , approveDomains: approveDomains
+  });
 
-app.listen(port, function () {
-  console.log(`Listening on port ${port}!`);
-});
-
-*/
-
-'use strict';
- 
-// returns an instance of node-greenlock with additional helper methods 
-var lex = require('greenlock-express').create({
-  // set to https://acme-v01.api.letsencrypt.org/directory in production 
-  server: 'https://acme-v01.api.letsencrypt.org/directory'
- 
-// If you wish to replace the default plugins, you may do so here 
-// 
-, challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
-, store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
- 
-// You probably wouldn't need to replace the default sni handler 
-// See https://git.daplie.com/Daplie/le-sni-auto if you think you do 
-//, sni: require('le-sni-auto').create({}) 
- 
-, approveDomains: approveDomains
-});
-
-function approveDomains(opts, certs, cb) {
-  // This is where you check your database and associated 
-  // email addresses with domains and agreements and such 
- 
- 
-  // The domains being approved for the first time are listed in opts.domains 
-  // Certs being renewed are listed in certs.altnames 
-  if (certs) {
-    opts.domains = certs.altnames;
+  function approveDomains(opts, certs, cb) {
+    // This is where you check your database and associated 
+    // email addresses with domains and agreements and such 
+  
+  
+    // The domains being approved for the first time are listed in opts.domains 
+    // Certs being renewed are listed in certs.altnames 
+    if (certs) {
+      opts.domains = certs.altnames;
+    }
+    else {
+      opts.email = 'ernesto.laval@gmail.com';
+      opts.agreeTos = true;
+    }
+  
+    // NOTE: you can also change other options such as `challengeType` and `challenge` 
+    // opts.challengeType = 'http-01'; 
+    // opts.challenge = require('le-challenge-fs').create({}); 
+  
+    cb(null, { options: opts, certs: certs });
   }
-  else {
-    opts.email = 'ernesto.laval@gmail.com';
-    opts.agreeTos = true;
-  }
- 
-  // NOTE: you can also change other options such as `challengeType` and `challenge` 
-  // opts.challengeType = 'http-01'; 
-  // opts.challenge = require('le-challenge-fs').create({}); 
- 
-  cb(null, { options: opts, certs: certs });
+
+  // handles acme-challenge and redirects to https 
+  require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
+    console.log("Listening for ACME http-01 challenges on", this.address());
+  });
+  
+  app.use('/', function (req, res) {
+    res.end('Hello, World!');
+  });
+  
+  // handles your app 
+  require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
+    console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+  });
+
+} else {
+
+  app.listen(port, function () {
+    console.log(`Listening on port ${port}!`);
+  });
+  
 }
 
-// handles acme-challenge and redirects to https 
-require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
-  console.log("Listening for ACME http-01 challenges on", this.address());
-});
- 
-app.use('/', function (req, res) {
-  res.end('Hello, World!');
-});
- 
-// handles your app 
-require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
-  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
-});
+
+
+
+
