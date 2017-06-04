4025afbfd7b48b0cf5054c59d4cc3455fa3a7fb5
Ernesto Laval <ernesto.laval@gmail.com>
2017-06-04 19:28:14 +0100

    Add https w/greenlock / lets encrypt

--- server3.js
+++ server3.js
@@ -1,87 +1,4 @@
 'use strict';
-var express = require('express');
-var app = express();
-var bodyParser  = require('body-parser');
-var morgan      = require('morgan');
-var mongoose    = require('mongoose');
-var fs = require('fs');
-var bearerToken = require("express-bearer-token");
-var cors = require('cors')
-var config = require('./config'); // get our config file
-
-// Use native promises
-mongoose.Promise = global.Promise;
-
-var schoolHandlers = require('./handlers')
-var comunaHandlers = require('./handlersComunas')
-var regionHandlers = require('./handlersRegion')
-
-var mongoDb = process.env.MONGO_DATABASE || config.database;
-console.log(mongoDb)
-mongoose.connect(mongoDb); // connect to database
-
-let port = 8021;
-
-app.use(morgan('dev'));
-app.use(cors());
-app.options('*', cors()); // to allow pre-flight cors 
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/config', function (req, res) {
-  res.send(config.database);
-});
-
-app.get('/schools', function (req, res) {
-  schoolHandlers.schools(req,res);
-});
-
-app.get('/education', function (req, res) {
-  schoolHandlers.education(req,res);
-});
-
-app.get('/comuna', function (req, res) {
-  comunaHandlers.getAllComunas(req,res);
-});
-
-app.get('/comuna/:comuna', function (req, res) {
-  comunaHandlers.getOneComuna(req,res);
-});
-
-
-app.get('/region', function (req, res) {
-  regionHandlers.getAllRegion(req,res);
-});
-
-app.get('/region/:region', function (req, res) {
-  regionHandlers.getOneRegion(req,res);
-});
-
-app.get('/pais', function (req, res) {
-  regionHandlers.getPais(req,res);
-});
-
-app.get('/pais/history', function (req, res) {
-  regionHandlers.getPaisHistory(req,res);
-});
-
-
-app.get('/matricula', function (req, res) {  
-  regionHandlers.matriculaHandler(req,res);
-});
-
-/*
-
-app.listen(port, function () {
-  console.log(`Listening on port ${port}!`);
-});
-
-*/
-
-'use strict';
  
 // returns an instance of node-greenlock with additional helper methods 
 var lex = require('greenlock-express').create({
@@ -127,6 +44,9 @@ require('http').createServer(lex.middleware(require('redirect-https')())).listen
   console.log("Listening for ACME http-01 challenges on", this.address());
 });
  
+ 
+ 
+var app = require('express')();
 app.use('/', function (req, res) {
   res.end('Hello, World!');
 });
